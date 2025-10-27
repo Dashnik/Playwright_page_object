b9d8a9884593b4df9ee229f983a1e07208d9c00f
Alex <a.okovalev@yandex.ru>
2025-10-27 11:03:58 +0100

    Feature: eslint config was set up.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
 import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default [
   ...tseslint.configs.recommended, // TypeScript recommended rules
+  eslintConfigPrettier,
 ];
